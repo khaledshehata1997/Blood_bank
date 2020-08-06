3bc2bc1ac0f8ca5e19f1ed2c28cd2dacb216975d
khaled shehata <khaledshehata492@gmail.com>
2020-08-06 21:38:22 +0200

    adding files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blood/provider/loginAndsignup.dart';
+import 'package:flutter_blood/screens/home.dart';
 import 'package:provider/provider.dart';
+import 'provider/loginAndsignup.dart';
 import 'provider/provider_data.dart';
-import 'screens/acoount.dart';
-import 'screens/choose.dart';
-import 'screens/home.dart';
-import 'screens/login.dart';
-import 'screens/need_blood.dart';
-import 'screens/signup.dart';
 
 void main () {
   runApp(
@@ -23,14 +18,10 @@ void main () {
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: 'Login' ,
+          initialRoute: 'Home' ,
           title: 'Blood bank',
           routes: {
-            'Login':(context)=>Login(),
-            'SignUp':(context)=>SignUp(),
-            'Choose':(context)=>Choose(),
-            'NeedBlood':(context)=>NeedBlood(),
-            'Account':(context)=>Account(),
+
             'Home':(context)=>Home(),
 
           },
